e3e4698b29a13571860ab065dbfe14dfef37081a
dzhu787451@gmail.com <derek.z2030@J6Y0V641RQ-Ziqi.local>
2026-07-09 19:57:06 +0900

    added lesson1

---
+++ lesson1/activity1.py
@@ -0,0 +1,11 @@
+print("My Name is Derek")
+print("Hello World")
+print("My Hobbies are playing video games, and playing baseball")
+sum=3+5
+print("The sum of 3 and 5 is", sum)
+num=int(input("Enter a number: "))
+percentage=3.14
+name="Derek"
+print("The number is", num)
+print(percentage)
+print(name)
