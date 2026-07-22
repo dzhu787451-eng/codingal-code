782989ef58b21306f364e0bfa3ab021e47d83d9c
dzhu787451-eng <dzhu787451@gmail.com>
2026-07-22 15:10:21 +0900

    added ACP

---
+++ codingal-code/lesson6-operators2/ACP-alphabet.py
@@ -0,0 +1,5 @@
+character=input("enter a character: ")
+if "A"<=character<="Z" or "a"<=character<="z":
+    print("your character is part of the alphabet")
+else:
+    print("your character is not part of the alphabet")
