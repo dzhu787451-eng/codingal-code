b6be78ee954d80632895986a231f260b2f5905e3
dzhu787451-eng <dzhu787451@gmail.com>
2026-07-24 16:49:51 +0900

    added acp-checkage

---
+++ codingal-code/lesson7-nestedif/ACP-checkage.py
@@ -0,0 +1,8 @@
+user_age=int(input("Please enter your age: "))
+if user_age>=10:
+    if user_age<=20:
+        print("You can join this class")
+    else:
+        print("You can't join this class")
+else:
+    print("You can't join this class")
