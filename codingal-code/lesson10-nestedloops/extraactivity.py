8bc8eac850771c5fad2ff8457f8a8e452da1a8d6
dzhu787451-eng <dzhu787451@gmail.com>
2026-07-30 20:10:26 +0900

    added lesson10

---
+++ codingal-code/lesson10-nestedloops/extraactivity.py
@@ -0,0 +1,5 @@
+adj=["red", "healthy", "blue", "yummy", "bright", "gross", "green", "yellow"]
+fruits=["lemon", "apple", "kiwi", "banana", "orange", "grapefruit", "grapes", "mangoes"]
+for i in adj:
+    for j in fruits:
+        print(i, j)
