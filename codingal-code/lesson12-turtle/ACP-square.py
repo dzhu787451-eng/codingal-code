644e3dfa4fc5f5c556ac5bccfb4eff91e9354f5b
dzhu787451-eng <dzhu787451@gmail.com>
2026-08-08 17:08:59 +0900

    added ACP-square

---
+++ codingal-code/lesson12-turtle/ACP-square.py
@@ -0,0 +1,9 @@
+import turtle
+turtle.Screen().setup(500,500)
+turtle.Screen().bgcolor("Green")
+turtle.title("This is a square")
+board=turtle.Turtle()
+for i in range(4):
+    board.forward(100)
+    board.right(90)
+turtle.done()
